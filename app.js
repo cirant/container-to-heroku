a69d9396563ac20083514ed44371b16415df899d
Pablo Cirant <cirant3002@gmail.com>
2019-12-31 21:47:10 -0300

    simple post

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+var PostSchema = mongoose.Schema({
+  title: String,
+  body: String,
+});
+
+var Post = mongoose.model('Post', PostSchema);
+
+dotenv.config();
 
 const app = express();
 
@@ -6,4 +17,22 @@ app.route('/').get((req, res) => {
   res.send('its\' work');
 })
 
-app.listen(process.env.PORT || 3000);
+app.route('/mongo').get(async (req, res) => {
+  try {
+    const post = await Post.create({
+      title: 'hola',
+      body: 'soy un body'
+    });
+    res.status(200).json({ response: 'ok', post });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+})
+
+
+mongoose.connect(process.env.MONGO_DB).then(() => {
+  console.log('db connected')
+  app.listen(process.env.PORT || 3000);
+}).catch(err => {
+  console.log(err);
+})
